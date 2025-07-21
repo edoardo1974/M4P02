278c60b75f8bc8ed485cee55a83b141c1c11bcc5
edoardo1974 <edoardo.mozzato@gmail.com>
2025-07-21 17:58:34 +0100

    Last Version

--- js/salvamentolocal.js
+++ js/salvamentolocal.js
@@ -1,18 +1,51 @@
+/**
+ * Stores the value "Jose" in localStorage under the key "M4_UD02_P02" if it does not already exist.
+ * Logs a success message if the value is stored.
+ * If the key already exists, logs an error and shows an alert to the user.
+ */
 function armazenaNovodado() {
-    localStorage.setItem("M4_UD02_P02", "Jose");
-    console.log("Dado armazenado com sucesso!");
+    if (localStorage.getItem("M4_UD02_P02") === null) {
+        localStorage.setItem("M4_UD02_P02", "Jose");
+        console.log("Dado armazenado com sucesso!");
+    }
+    else{
+        console.error("LocalStorage já contém um valor.");
+        alert("O valor já foi armazenado.");
+        return; 
+    }
 }
 
+/**
+ * Stores the current timestamp in sessionStorage under the key "Date" if it does not already exist.
+ * If the key already exists, logs an error and shows an alert to the user.
+ */
 function armazenaSegundoDado() {
-    const tempo = new Date().getTime();
-    sessionStorage.setItem("Date", tempo);
-    console.log("Segundo dado armazenado com sucesso!");
+    if (sessionStorage.getItem("Date") === null) {
+        const tempo = new Date().getTime();
+        sessionStorage.setItem("Date", tempo);
+        console.log("Segundo dado armazenado com sucesso!");
+    }
+    else{   
+        console.error("SessionStorage já contém um valor.");
+        alert("O valor já foi armazenado.");
+        return;
+    }
 }
 
+/**
+ * Retrieves and displays stored data from localStorage and sessionStorage.
+ * - Gets the key "M4_UD02_P02" from localStorage and its associated value.
+ * - Gets the key "Date" from sessionStorage and its associated value.
+ * - Logs the retrieved data to the console.
+ * - Appends the data to the HTML element with id 'output'.
+ */
 function imprimirDadosArmazenados() {
-    const dadoLocal = localStorage.getItem("M4_UD02_P02");
-    const dadoSession = sessionStorage.getItem("Date");
+    const nome = localStorage.key("M4_UD02_P02");
+    const dadoLocal = localStorage.getItem(nome);
+    const dadoSession = sessionStorage.key("Date");
+    const dadoSessionValue = sessionStorage.getItem(dadoSession);
     console.log("Dados armazenados:");
     console.log("LocalStorage:", dadoLocal);
-    console.log("SessionStorage:", dadoSession);
+    document.getElementById('output').innerHTML += `LocalStorage -- key: ${nome} value: ${dadoLocal}  <br>`;
+    document.getElementById('output').innerHTML += `SessionStorage -- key: ${dadoSession} value: ${dadoSessionValue}<br>`;
 }
